9dc0ea7c107191ee858924e340451321e403f1e0
Martin Hoff Teigen <martin@teigen.dev>
2026-07-11 01:01:38 +0100

    Shorten the interval seconds

--- src/configs/config.py
+++ src/configs/config.py
@@ -17,8 +17,8 @@ HTTP_PORT = 8000
 
 # Time between captures when capture mode is active.
 IDLE_CAPTURE_INTERVAL_SECONDS = 300  # User is not moving, so we can take photos less frequently.
-DEFAULT_CAPTURE_INTERVAL_SECONDS = 120  # User is moving, so we can take photos more frequently.
-VIDEO_CAPTURE_INTERVAL_SECONDS = 180  # User is moving a lot, so we can capture videos.
+DEFAULT_CAPTURE_INTERVAL_SECONDS = 60  # User is moving, so we can take photos more frequently.
+VIDEO_CAPTURE_INTERVAL_SECONDS = 60  # User is moving a lot, so we can capture videos.
 
 # Length of each video clip when motion is detected.
 VIDEO_DURATION_SECONDS = 10
